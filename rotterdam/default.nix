a8657c9f5a279e1e8125bac13c0ad73e1cf0d981
Rafael Oliveira <rafdev.x@gmail.com>
2025-08-08 22:47:28 +0100

    rotterdam: use flameshot for screenshots

--- rotterdam/default.nix
+++ rotterdam/default.nix
@@ -388,6 +388,24 @@
         ];
       };
     };
+
+    dconf.settings = {
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        custom-keybindings = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        ];
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        name = "Flameshot Screenshot";
+        binding = "Print";
+        # ^ this will clash with built-in "Interactive Screenshot", so for it to
+        # work it might be necessary to go to Settings > Keyboard > View and
+        # Customize Shortcuts > Custom Shortcuts > Flameshot Screenshots, then
+        # set the binding to backspace (disabled), and then re-set it to Print
+        command = ''script --command "flameshot gui" /dev/null'';
+      };
+    };
   };
 
   fonts.packages =
