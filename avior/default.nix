d455c29002818f593deb059c6ca67234204ab2db
Rafael Oliveira <rafdev.x@gmail.com>
2025-08-09 23:04:40 +0100

    avior: install nginx

--- avior/default.nix
+++ avior/default.nix
@@ -48,6 +48,42 @@
     enableJIT = true;
   };
 
+  services.nginx = {
+    enable = true;
+
+    # reload (vs. restart) when configuration changes
+    enableReload = true;
+
+    # enable compression
+    recommendedZstdSettings = true;
+    recommendedGzipSettings = true;
+    recommendedBrotliSettings = true;
+
+    # other recommended settings
+    recommendedOptimisation = true;
+    recommendedTlsSettings = true;
+    recommendedProxySettings = true;
+    recommendedUwsgiSettings = true;
+
+    # reject connections to unknown virtual hosts
+    virtualHosts."_" = {
+      default = true;
+      rejectSSL = true;
+      locations."/" = {
+        return = "444"; # nginx doesn't respond and drops connection
+      };
+    };
+  };
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "lets-encrypt@rso.pt";
+  };
+
   programs.zsh.enable = true;
   programs.vim = {
     enable = true;
